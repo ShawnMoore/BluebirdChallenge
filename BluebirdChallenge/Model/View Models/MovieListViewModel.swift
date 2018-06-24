31f16582570f20fb12f49da63489d65c02a747ab
ShawnMoore <sm5@me.com>
2018-06-23 17:35:10 -0700

    Initial View Setup

---
+++ BluebirdChallenge/Model/View Models/MovieListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  MovieListViewModel.swift
+//  BluebirdChallenge
+//
+//  Created by Shawn Moore on 6/23/18.
+//  Copyright © 2018 Shawn Moore. All rights reserved.
+//
+
+import Foundation
+
